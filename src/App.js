7d572471e438c555e163261cb0e0fad37a93b4bd
sargis davtyan100 <sargis.davtyan100@gmail.com>
2021-08-27 18:20:48 +0400

    bootcamp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Menu } from './menu/Menu';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import {Home} from './homePage/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path = "/" exact component = {Home}/>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/homePage/Home.js
@@ -0,0 +1,11 @@
+import { Menu } from '../menu/Menu'
+
+export function Home() {
+  return (
+    <>
+      <Menu />
+      <div className='parentHome'>
+      </div>
+    </>
+  )
+}
